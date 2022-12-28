bec55bd2cc20efa6e2cbaa1b93430388b517e4c5
Aslıhan <82841457+AslihanAkbulutt@users.noreply.github.com>
2022-12-28 23:28:00 +0300

    application api services eklendi

---
+++ WebProject/Controllers/ApplicationAPIController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Data;
+using WebProject.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ApplicationAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ApplicationAPIController>
+        [HttpGet]
+        public async Task<ActionResult<List<Application>>> Get()
+        {
+            var m = await _context.Applications.ToListAsync();
+            if (m is null)
+            {
+                return NoContent();
+            }
+            return m;
+        }
+
+        // GET api/<ApplicationAPIController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Application>> Get(int id)
+        {
+            var m = await _context.Applications.FirstOrDefaultAsync(x => x.Id == id);
+            if (m is null)
+            {
+                return NoContent();
+            }
+            return m;
+        }
+
+        // POST api/<ApplicationAPIController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Application m)
+        {
+            _context.Applications.Add(m);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        // PUT api/<ApplicationAPIController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Application m)
+        {
+            var m1 = _context.Applications.FirstOrDefault(x => x.Id == id);
+            if (m1 is null)
+            {
+                return NotFound();
+            }
+            m1.UserName = m.UserName;
+            m1.AnimalId = m.AnimalId;
+            m1.Name = m.Name;
+            m1.LastName = m.LastName;
+            m1.Adress = m.Adress;
+            m1.PhoneNo = m.PhoneNo;
+            m1.Explanation = m.Explanation;
+            _context.Update(m1);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        // DELETE api/<ApplicationAPIController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var m1 = _context.Applications.FirstOrDefault(x => x.Id == id);
+            if (m1 is null)
+            {
+                return NotFound();
+            }
+
+            _context.Applications.Remove(m1);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebProject/Controllers/CallApplicationAPIController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using Newtonsoft.Json;
+using System.Data;
+using System.Text;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CallApplicationAPIController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Application> applications = new List<Application>();
+            var hhtc = new HttpClient();
+            var response = await hhtc.GetAsync("https://localhost:7224/api/ApplicationAPI");
+            string resString = await response.Content.ReadAsStringAsync();
+            applications = JsonConvert.DeserializeObject<List<Application>>(resString);
+            return View(applications);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            Application application = new Application();
+            var hhtc = new HttpClient();
+            var response = await hhtc.GetAsync("https://localhost:7224/api/ApplicationAPI/" + id);
+            string resString = await response.Content.ReadAsStringAsync();
+            application = JsonConvert.DeserializeObject<Application>(resString);
+            return View(application);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            Application application= new Application();
+            var hhtc = new HttpClient();
+            var response = await hhtc.DeleteAsync("https://localhost:7224/api/ApplicationAPI/" + id);
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Application application = new Application();
+            var hhtc = new HttpClient();
+            var response = await hhtc.GetAsync("https://localhost:7224/api/ApplicationAPI/" + id);
+            string resString = await response.Content.ReadAsStringAsync();
+            application = JsonConvert.DeserializeObject<Application>(resString);
+            return View(application);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Application m, int id)
+        {
+
+            var hhtc = new HttpClient();
+            var json = new JavaScriptSerializer().Serialize(m);
+            // var sp = JsonSerializer.Serialize(json);
+
+            StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var result = hhtc.PutAsync("https://localhost:7224/api/ApplicationAPI/" + id, stringContent).Result;
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Application m)
+        {
+
+            var hhtc = new HttpClient();
+            var json = new JavaScriptSerializer().Serialize(m);
+            // var sp = JsonSerializer.Serialize(json);
+
+            StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var result = hhtc.PostAsync("https://localhost:7224/api/ApplicationAPI/", stringContent).Result;
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
